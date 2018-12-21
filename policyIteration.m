c7c2f2fccb6e5423d3574522bed6814028abd9d4
John Wennerstrum <jwennerstrum3@gatech.edu>
2018-12-21 13:25:59 -0600

    Adding policyIteration function and testMap for testing purposes

---
+++ policyIteration.m
@@ -0,0 +1,11 @@
+function policyIteration(map, varargin)
+% varargin(1) - default value of each state
+% varargin(2) - Jx2 array with x-y locations of terminating states
+% varargin(2) - Nx2 array with x-y locations of custom reward states
+% varargin(3) - Nx2 array with rewards for custom rewards states
+
+
+
+
+
+end
---
+++ testMap.m
@@ -0,0 +1,6 @@
+map = [[1, 0, 0, 0]; ...
+       [0, 0, 1, 1]];
+   
+[r, c] = size(map);
+values = zeros(r, c);
+
